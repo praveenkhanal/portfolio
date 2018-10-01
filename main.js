f27c2a9d1e0b3650aab3efe8fd507f64ec786fbb
praveenkhanal <praveenkhanal@Praveens-MacBook-Pro.local>
2018-09-30 21:47:24 -0700

    final

--- main.js
+++ main.js
@@ -38,5 +38,15 @@ $(function() {
 });
 
 
+// $(document).ready(function() {
 
+//     var headerHeight = $('header').outerHeight();
+//     $('.slide-section').click(function(e){
+//         var linkHref = $(this).attr('href');
+//         $('html, body').animate({
+//             scrollTop: $(linkHref).offset().top - headerHeight
+//         }, 1000);
+//         e.preventDefault();
+//         })
+// });
 
